0b8bdb76df0aebb4febfc41c4190d66cf80c02ec
Leander Berg <jelliebien@gmail.com>
2023-04-10 23:22:51 +0200

    debugging

--- routes/app/admin/location_post.js
+++ routes/app/admin/location_post.js
@@ -1,18 +1,14 @@
-const { ExamLocation } = requiremain('./db/db')
+const { Exam, ExamLocation } = requiremain('./db/db')
 
 module.exports = async (req, res) => {
-  try {
-    const location = await ExamLocation.findByPk(req.params.id)
-    if (parseInt(req.body.joinId)) {
-      await Exam.update({'ExamLocationId': parseInt(req.body.joinId)}, {where: {'ExamLocationId': req.params.id}})
-      await location.destroy()
-    }
-    if (req.body.newname) 
-      await ExamLocation.update({'name': req.body.newname}, {where: {'id': req.params.id}})
-    if (req.body.delete)
-      await location.destroy()
-  } catch (e) {
-    res.status(200).send(e)
+  const location = await ExamLocation.findByPk(req.params.id)
+  if (parseInt(req.body.joinId)) {
+    await Exam.update({'ExamLocationId': parseInt(req.body.joinId)}, {where: {'ExamLocationId': req.params.id}})
+    await location.destroy()
   }
+  if (req.body.newname) 
+    await ExamLocation.update({'name': req.body.newname}, {where: {'id': req.params.id}})
+  if (req.body.delete)
+    await location.destroy()
   res.redirect('/app/admin/locations')
 }
