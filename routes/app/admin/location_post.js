0cc7ae25c49ef1f09e108553cd64259376842f8a
Leander Berg <jelliebien@gmail.com>
2023-04-10 23:19:27 +0200

    debugging

--- routes/app/admin/location_post.js
+++ routes/app/admin/location_post.js
@@ -1,14 +1,18 @@
 const { ExamLocation } = requiremain('./db/db')
 
 module.exports = async (req, res) => {
-  const location = await ExamLocation.findByPk(req.params.id)
-  if (parseInt(req.body.joinId)) {
-    await Exam.update({'ExamLocationId': parseInt(req.body.joinId)}, {where: {'ExamLocationId': req.params.id}})
-    await location.destroy()
+  try {
+    const location = await ExamLocation.findByPk(req.params.id)
+    if (parseInt(req.body.joinId)) {
+      await Exam.update({'ExamLocationId': parseInt(req.body.joinId)}, {where: {'ExamLocationId': req.params.id}})
+      await location.destroy()
+    }
+    if (req.body.newname) 
+      await ExamLocation.update({'name': req.body.newname}, {where: {'id': req.params.id}})
+    if (req.body.delete)
+      await location.destroy()
+  } catch (e) {
+    res.status(200).send(e)
   }
-  if (req.body.newname) 
-    await ExamLocation.update({'name': req.body.newname}, {where: {'id': req.params.id}})
-  if (req.body.delete)
-    await location.destroy()
   res.redirect('/app/admin/locations')
 }
