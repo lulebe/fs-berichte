0b884d3e16585c4da818896e05be369930fa2a22
Leander Berg <reg@lulebe.net>
2023-10-06 12:17:30 +0200

    fixes: DL btn mainpage, research input validation

--- db/init-db.js
+++ db/init-db.js
@@ -1,8 +1,10 @@
 const db = require('./db')
 const bcrypt = require('bcryptjs')
 const config = require('../config')
+const { setSetting, SETTINGS_KEYS } = require('./stored_settings')
 
 db.init().then(() => {
+    setSetting(SETTINGS_KEYS.RESEARCH_REPORTS_PUBLIC, false)
     const password = bcrypt.hashSync("testpw", bcrypt.genSaltSync(config.SALT_ROUNDS))
     db.User.create({email: "test@example.com", activated: true, authorized: true, isAdmin: true, password})
     .then(() => {
