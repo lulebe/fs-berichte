ff407c0a794d3b444d99cfa5fb66ddfaa046a7fc
Leander Berg <reg@lulebe.net>
2023-10-25 19:11:22 +0200

    fixed merge examiner form autocomplete

--- assets/autocomplete.js
+++ assets/autocomplete.js
@@ -3,8 +3,11 @@ const autocompleteMenu = document.createElement('div')
 const autocompleteList = document.createElement('ul')
 autocompleteList.addEventListener('mousedown', e => {
   attachedField.value = e.target.textContent
+  //fill hidden field
   attachedField.previousElementSibling.value = e.target.dataset.id
-  attachedField.nextElementSibling.innerText = ''
+  //clear warning
+  if (autocompleteFeedback)
+    attachedField.nextElementSibling.innerText = ''
   attachedField.blur()
   resetAutocomplete()
 })
@@ -17,12 +20,14 @@ for (let i = 0; i < autocompleteFields.length; i++) {
   autocompleteField.addEventListener('blur', () => hideAutocomplete(e))
   autocompleteField.addEventListener('focus', showAutocomplete)
   autocompleteField.addEventListener('input', e => {
+    //clear hidden field
     e.target.previousElementSibling.value = ''
     showAutocomplete(e)
   })
 }
 
 function hideAutocomplete (e) {
+  //show warning
   if (autocompleteFeedback && attachedField.previousElementSibling.value == '') {
     attachedField.nextElementSibling.innerText = attachedField.value + ' wird neu angelegt. Wähle wenn möglich einen der Vorschläge.'
   }
@@ -41,7 +46,8 @@ function showAutocomplete (e) {
   }
   attachedField = e.target
   fillMenu(e.target.value, atData[e.target.dataset.autocomplete])
-  attachedField.nextElementSibling.innerText = ''
+  if (autocompleteFeedback)
+    attachedField.nextElementSibling.innerText = ''
   e.target.parentElement.appendChild(autocompleteMenu)
 }
 
