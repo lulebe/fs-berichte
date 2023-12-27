ccda29eec3234c505447a432fb31c03abb882ffe
Leander Berg <reg@lulebe.net>
2023-12-27 18:28:48 +0100

    pending Petitions on Admin page

--- routes/app/admin/petitions.js
+++ routes/app/admin/petitions.js
@@ -6,5 +6,6 @@ const { getSetting, SETTINGS_KEYS } = requiremain('./db/stored_settings')
 module.exports = async (req, res) => {
   res.tmplOpts.tags = await Tag.findAll({order: [['name', 'ASC']]})
   res.tmplOpts.petitionsRequireAdminConfirmation = (await getSetting(SETTINGS_KEYS.PETITIONS_REQUIRE_ADMIN_CONFIRMATION)) === '1'
+  res.tmplOpts.petitionsAwaitingConfirmation = await Petition.findAll({where: {status: 0}})
   tmpl.render('app/admin/petitions.twig', res.tmplOpts).then(rendered => res.end(rendered))
 }
