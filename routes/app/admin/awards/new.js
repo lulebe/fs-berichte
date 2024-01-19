75bcfaa52ab6eab359d4829f66223794cc36a1b4
Leander Berg <reg@lulebe.net>
2024-01-19 14:02:42 -0800

    added new award page

--- routes/app/admin/awards/new.js
+++ routes/app/admin/awards/new.js
@@ -3,5 +3,9 @@ const tmpl = requiremain('./templates')
 const {  } = requiremain('./db/db')
 
 module.exports = async (req, res) => {
+  const d = new Date()
+  res.tmplOpts.currentDate = d.getUTCFullYear() + "-" + (d.getUTCMonth()+1+"").padStart(2, "0") + "-" + (""+d.getUTCDate()).padStart(2, "0")
+  d.setMonth(d.getMonth() + 2)
+  res.tmplOpts.deadlineDate = d.getUTCFullYear() + "-" + (d.getUTCMonth()+1+"").padStart(2, "0") + "-" + (""+d.getUTCDate()).padStart(2, "0")
   tmpl.render('app/admin/awards/new.twig', res.tmplOpts).then(rendered => res.end(rendered))
 }
