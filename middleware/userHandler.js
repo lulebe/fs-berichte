fe5ad862768b4a4669e7a265c55546eb7b99946a
Leander Berg <reg@lulebe.net>
2024-01-24 15:46:42 -0800

    random visual changes

--- middleware/userHandler.js
+++ middleware/userHandler.js
@@ -1,4 +1,4 @@
-const { User } = requiremain('./db/db')
+const { User, Award, AwardVote } = requiremain('./db/db')
 
 module.exports = async function (req, res, next) {
   if (!req.session.userId) {
@@ -10,6 +10,7 @@ module.exports = async function (req, res, next) {
     req.session.destroy()
     return res.redirect('/')
   }
+  res.tmplOpts.hasNewAward = (await Award.findAll({where: {status: Award.STATUS.PUBLISHED}, attributes: ['id'], include: {model: AwardVote, where: {UserId: req.user.id}, required: false}})).filter(a => a.AwardVotes.length === 0)
   res.tmplOpts.isLoggedIn = true
   res.tmplOpts.user = req.user
   res.tmplOpts.isAdmin = req.user.isAdmin
--- routes/app/awards/awards.js
+++ routes/app/awards/awards.js
@@ -1,8 +1,9 @@
+const { Op } = require('sequelize')
 const tmpl = requiremain('./templates')
 
 const { Award } = requiremain('./db/db')
 
 module.exports = async (req, res) => {
-  res.tmplOpts.awards = await Award.findAll()
+  res.tmplOpts.awards = await Award.findAll({where: {status: {[Op.gte]: Award.STATUS.PUBLISHED}}})
   tmpl.render('app/awards/awards.twig', res.tmplOpts).then(rendered => res.end(rendered))
 }
