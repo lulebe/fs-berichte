ac686e33cc4af7d0dc3be6bcea7b5a345e6703a8
Leander Berg <reg@lulebe.net>
2024-01-24 00:12:33 -0800

    profile redesign

--- routes/app/profile.js
+++ routes/app/profile.js
@@ -1,7 +1,7 @@
 const tmpl = requiremain('./templates')
 const Sequelize = require('sequelize')
 
-const { Exam, SubjectExam, ResearchReport, Subject, Examiner, ExamLocation } = requiremain('./db/db')
+const { Exam, SubjectExam, ResearchReport, Subject, Examiner, ExamLocation, Petition, PetitionComment } = requiremain('./db/db')
 
 module.exports = async (req, res) => {
 
@@ -12,7 +12,10 @@ module.exports = async (req, res) => {
     r.subjects = r.SubjectExams.map(se => se.Subject.name).join(', ')
   })
   res.tmplOpts.user = req.user
-  res.tmplOpts.research = await ResearchReport.findAll({where: {UserId: req.user.id}})
   res.tmplOpts.reports = results
+  res.tmplOpts.research = await ResearchReport.findAll({where: {UserId: req.user.id}})
+  res.tmplOpts.petitions = await Petition.findAll({where: {UserId: req.user.id}})
+  res.tmplOpts.petitionComments = await PetitionComment.findAll({where: {UserId: req.user.id}, include: [Petition]})
+  res.tmplOpts.noContent = res.tmplOpts.research.length === 0 && res.tmplOpts.reports.length === 0 && res.tmplOpts.petitions.length === 0 && res.tmplOpts.petitionComments.length === 0
   tmpl.render('app/profile.twig', res.tmplOpts).then(rendered => res.end(rendered))
 }
