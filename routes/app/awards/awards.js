c3415a8f8fe66c64ab8d51ffe5bb2e7bd128a345
Leander Berg <reg@lulebe.net>
2024-01-25 19:01:33 -0800

    awardDescription markdown render

--- routes/app/awards/awards.js
+++ routes/app/awards/awards.js
@@ -1,4 +1,5 @@
 const { Op } = require('sequelize')
+const md = require('markdown-it')()
 
 const tmpl = requiremain('./templates')
 const { Award } = requiremain('./db/db')
@@ -6,6 +7,6 @@ const { getSetting, SETTINGS_KEYS } = requiremain('./db/stored_settings')
 
 module.exports = async (req, res) => {
   res.tmplOpts.awards = await Award.findAll({where: {status: {[Op.gte]: req.user.isAdmin ? 0 : 1}}, order: [['createdAt', 'desc']]})
-  res.tmplOpts.awardDescription = await getSetting(SETTINGS_KEYS.AWARD_DESCRIPTION)
+  res.tmplOpts.awardDescription = md.render(await getSetting(SETTINGS_KEYS.AWARD_DESCRIPTION))
   tmpl.render('app/awards/awards.twig', res.tmplOpts).then(rendered => res.end(rendered))
 }
