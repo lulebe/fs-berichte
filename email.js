d4270399d10ec3a025f47b9b6f44ab804dabbd22
Leander Berg <reg@lulebe.net>
2024-02-06 00:30:34 +0100

    email test

--- email.js
+++ email.js
@@ -17,12 +17,16 @@ module.exports = async function (email, subject, text, html) {
     console.log(email, subject, text)
     return true
   }
-  let mailStatus = await transporter.sendMail({
-    from: config.MAIL_SENDER,
-    to: email,
-    subject,
-    text: 'Hallo,\n\n' + text + '\n\nViele Grüße,\ndas Fachschaftsteam Lehre',
-    html: 'Hallo,<br><br>' + html + '<br><br>Viele Grüße,<br>das Fachschaftsteam Lehre'
-  })
-  return mailStatus
+  try {
+    let mailStatus = await transporter.sendMail({
+      from: config.MAIL_SENDER,
+      to: email,
+      subject,
+      text: 'Hallo,\n\n' + text + '\n\nViele Grüße,\ndas Fachschaftsteam Lehre',
+      html: 'Hallo,<br><br>' + html + '<br><br>Viele Grüße,<br>das Fachschaftsteam Lehre'
+    })
+    return mailStatus
+  } catch (e) {
+    return e
+  }
 }
