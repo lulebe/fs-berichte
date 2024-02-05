8359e66580bf26c623873932b7b33f608d6a6748
Leander Berg <reg@lulebe.net>
2024-02-06 00:25:07 +0100

    forgotpw changed

--- routes/forgotpwd.js
+++ routes/forgotpwd.js
@@ -12,14 +12,11 @@ module.exports = async (req, res) => {
   const foundUser = await User.findOne({where: {email: req.body.email.toLowerCase()}})
   if (!foundUser)
     return res.redirect('/?status=3')
-  const pw = generator.generate({length: 10, numbers: true})
-  foundUser.password = await bcrypt.hash(pw, await bcrypt.genSalt(config.SALT_ROUNDS))
-  await foundUser.save()
-  await sendEmail(foundUser.email, pw)
+  await sendEmail(foundUser)
   res.redirect('/?status=2')
 }
 
-async function sendEmail (email, pw) {
+async function sendEmail (user) {
   const jwt = await signJWT({id: user.id, password: user.password}, config.JWT_SECRET, {expiresIn: '3 days'})
   const url = `${config.ROOT_URL}/recover?token=${jwt}`
   return mailer(
