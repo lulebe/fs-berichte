a452ff5ecf2ab48011eec89611ac2988ea0b8ad7
Leander Berg <reg@lulebe.net>
2024-02-13 04:40:49 +0100

    form not found redirect

--- routes/app/form.js
+++ routes/app/form.js
@@ -4,5 +4,6 @@ const { Form } = requiremain('./db/db')
 
 module.exports = async (req, res) => {
   res.tmplOpts.form = await Form.findByPk(req.params.id)
+  if (!res.tmplOpts.form) return res.redirect('/app/forms')
   tmpl.render('app/form.twig', res.tmplOpts).then(rendered => res.end(rendered))
 }
