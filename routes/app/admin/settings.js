eeb8fc4f01b96f7f0e21aaaa175eef97a66067a1
Leander Berg <reg@lulebe.net>
2024-02-16 22:44:09 +0100

    activeadmintab settings

--- routes/app/admin/settings.js
+++ routes/app/admin/settings.js
@@ -5,5 +5,6 @@ const config = requiremain('./config')
 
 module.exports = async (req, res) => {
   res.tmplOpts.settings = await Settings.findAll()
+  res.tmplOpts.activeAdminTab = "users"
   tmpl.render('app/admin/settings.twig', res.tmplOpts).then(rendered => res.end(rendered))
 }
