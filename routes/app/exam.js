e96077408f03506c1c3b7afdc488b5fb3165e279
Leander Berg <reg@lulebe.net>
2024-02-26 00:37:48 +0100

    markdown in reports

--- routes/app/exam.js
+++ routes/app/exam.js
@@ -1,11 +1,15 @@
 const Cookies = require('cookies')
 const tmpl = requiremain('./templates')
-
+const md = requiremain('./markdownrender')
 const { Exam, ExamLocation, SubjectExam, Subject, Examiner, User } = requiremain('./db/db')
 
 module.exports = async (req, res) => {
   const exam = await Exam.findByPk(req.params.id, {include: [{model: SubjectExam, include: [Subject, Examiner]}, ExamLocation, User]})
   if (!exam) return res.redirect('/app/exam')
+  exam.commentRendered = md(exam.comment)
+  exam.SubjectExams.forEach(se => {
+    se.reportRendered = md(se.report)
+  })
   const cookies = new Cookies(req, res)
   let cookie = [req.params.id, ...(cookies.get('exams') ? cookies.get('exams').split(',') : [])]
   cookie = [...new Set(cookie)].slice(-5).join(',')
